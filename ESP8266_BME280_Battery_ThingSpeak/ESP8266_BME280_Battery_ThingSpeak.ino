12b295f362deed21eb1396e4b5497dc8eadfba56
Nikolay Georgiev <ngeorgiev@lelypartners.com>
2023-11-28 18:27:17 +0200

    Adding some functionallity and fixing some stuff

--- ESP8266_BME280_Battery_ThingSpeak/ESP8266_BME280_Battery_ThingSpeak.ino
+++ ESP8266_BME280_Battery_ThingSpeak/ESP8266_BME280_Battery_ThingSpeak.ino
@@ -1,112 +1,42 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
 #include "wifi_credentials.h"
 #include <ThingSpeak.h>
 #define sensor_address 0x76 // Adafruit devices use 0x77 all others use 0x76
+#define SLEEP_TIME 3e8 // // 3e8 is 5 minutes
+#define DEBUG 0 //debug = 1 -> enable 
 
-//---------- I2C mode and BME object
+//---------- BME object
 Adafruit_BME280 bme;
+
+int analogInPin  = A0;    // Analog input pin
 
 //--------- Wi-Fi settings
 WiFiClient  client;
 const char* ssid = WIFISSID;
 const char* password = PASSWORD;
-String newHostname = "Battery_Weather_Station";
+String newHostname = "NDK_Weather_Station";
 
 //--------ThingsSpeak channel settings
-unsigned long int myChannelNumber = 123456789;
-const char* myWriteAPIKey = "";
+unsigned long int myChannelNumber = MYCHANNUM;
+const char* myWriteAPIKey = APIWRITEKEY;
 
 // Variables for sensor readings
-float tempC;
-float humidity;
-float pressureAbs;
+float tempC, humidity, pressureAbs, pressureSea;
+int altitude = 557;
+float b = 17.62;
+float c = 243.12;
+float gamma_var, dewpoint, humidityAbs;
+// Battery level variables
+int sensorValue;          // Analog Output of Sensor
+float voltage;
+float calibration = 0.077; // Accuracy
+int bat_percentage;
+float pressureSea_cal = 1.0;
 
-// Activation of printValues() function for testing/debugging into the Serial Monitor
-bool debug = 0; //debug = 1 -> enable 
-
-// Initialization of BME280 sensor
-void initBME280() { 
-    if (!bme.begin(sensor_address)) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-    }
-}
-
-// Getting data from BME280
-void getDataBME280() {
-  tempC = bme.readTemperature();
-  pressureAbs = (bme.readPressure()/ 100.0F);
-  humidity = bme.readHumidity(); 
-     
-}
-
-// Printing the data into the Serial Monitor
-void printValues() {
-  if (debug) {
-   //Data from BME280  
-   Serial.println("Data from BME280 Sensor:");
-   Serial.printf("Temperature = %.1f ºC \n", tempC);
-   Serial.printf("Pressure = %.1f hPa \n", pressureAbs);
-   Serial.printf("Humidity = %.1f %% \n", humidity);   
-  }
-}
-
-void connectWiFi() {
-
-  // Connect to Wifi.
-  Serial.println("WiFi Status:");
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  // WiFi fix: https://github.com/esp8266/Arduino/issues/2186
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_OFF);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  unsigned long wifiConnectStart = millis();
-
-  while (WiFi.status() != WL_CONNECTED) {
-    // Check to see if
-    if (WiFi.status() == WL_CONNECT_FAILED) {
-      Serial.println("Failed to connect to WiFi. Please verify credentials: ");
-      delay(10000);
-    }
-
-    delay(500);
-    Serial.print(".");
-    // Only try for 5 seconds.
-    if (millis() - wifiConnectStart > 15000) {
-      Serial.println("Failed to connect to WiFi");
-      return;
-    }
- }
-}
-
-// Pushing data to Thingspeak.com
-void thingSpeak() {
-  // set the fields with the values
-    ThingSpeak.setField(1, tempC);
-    ThingSpeak.setField(2, humidity);
-    ThingSpeak.setField(3, pressureAbs);
-            
-    // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
-    // pieces of information in a channel.  Here, we write to field 1.
-    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-
-    if(x == 200){
-      Serial.println("Channel updated successfully into Thingspeak.");
-      Serial.println("============================================"); 
-    }
-    else{
-      Serial.println("Problem updating channel. HTTP error code " + String(x));
-    }
-}
+// Error codes
+int error = 0;
 
 // Function for sleeping the BME280 sensor
 void BME280_Sleep(int device_address) {
@@ -123,29 +53,167 @@ void BME280_Sleep(int device_address) {
   Wire.endTransmission();
 }
 
+void connectWiFi() {
+  // Connect to Wifi.
+  Serial.print("Connecting to: ");
+  Serial.println(ssid);
+  WiFi.hostname(newHostname);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  unsigned long wifiConnectStart = millis();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    // Check to see if
+    if (WiFi.status() == WL_CONNECT_FAILED) {
+      Serial.println("Failed to connect to WiFi. Please verify credentials!");
+      delay(1000);
+    }
+
+    delay(500);
+    Serial.print(".");
+    
+    // Only try for 20 seconds.
+    if (millis() - wifiConnectStart > 20000) {
+      Serial.println();
+      Serial.println("Failed to connect to WiFi");
+      ESP.deepSleep(SLEEP_TIME); // Going to deep sleep if there is not WiFi network available at the moment
+    }
+ }
+ Serial.println();
+}
+
+// Initialization of BME280 sensor and sending error if it is not present
+void initBME280() { 
+    unsigned long init_sensor = millis();
+
+    if (!bme.begin(sensor_address)) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      BME280_Sleep(sensor_address);  //Sleeping the sensor (don't know if it is needed)
+      error = 1; // No sensor detected
+      ThingSpeak.begin(client); // Initialize ThingSpeak
+      connectWiFi(); // Connect to WiFi to transming the error for not operational sensor
+      ThingSpeak.setField(7, error); // Pushing error to the error field in ThingSpeak
+      
+      int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+      if(x == 200){
+        Serial.println("Error pushed to Thingspeak!");
+        Serial.println("============================================"); 
+      }
+      else{
+        Serial.println("Problem updating channel. HTTP error code " + String(x));
+      }
+      Serial.println("Going into deep sleep for 5 minutes!");
+      ESP.deepSleep(SLEEP_TIME); // 30e8 is 5 minutes
+    }
+}
+
+// Getting data from BME280
+void getDataBME280() {
+  tempC = bme.readTemperature();
+  pressureAbs = (bme.readPressure()/ 100.0F);
+  humidity = bme.readHumidity();
+
+  //Calculate reltive pressure from absolute  
+  pressureSea = (((pressureAbs)/pow((1-((float)(altitude))/44330), 5.255))) - pressureSea_cal; // Calculating the sea level pressure (Relative pressure)
+
+  //Calculate dewpoint
+  gamma_var = (b*tempC) / (c + tempC) + log(humidity/100.0);
+  dewpoint = (c * gamma_var) / (b - gamma_var);
+
+  //Calculate absolute humidity from relative
+  humidityAbs = (6.112*exp((b*tempC)/(c + tempC))*humidity*2.1674) / (273.15 + tempC);
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void batteryLevel() {
+  sensorValue = analogRead(analogInPin); //0 - 1023 return value
+  voltage = ((((float)sensorValue *4.2) / 1024) - calibration);
+  if (voltage < 0){
+    voltage = 0;
+  }
+  bat_percentage = mapfloat(voltage, 3.3, 4.2, 0, 100); //2.8V as Battery Cut off Voltage & 4.2V as Maximum Voltage
+
+  if (bat_percentage >= 100)
+  {
+    bat_percentage = 100;
+  }
+
+  if (bat_percentage <= 0)
+  {
+    bat_percentage = 1;
+  }
+}
+
+// Printing the data into the Serial Monitor
+void printValues() {
+  if (DEBUG) {
+   //Data from BME280  
+   Serial.println("Data from BME280 Sensor:");
+   Serial.printf("Temperature = %.1f ºC \n", tempC);
+   Serial.printf("Abs. Pressure = %.1f hPa \n", pressureAbs);
+   Serial.printf("Sea level Pressure = %.1f hPa \n", pressureSea);
+   Serial.printf("Humidity = %.1f %% \n", humidity);
+   Serial.printf("Abs. Humidity = %.1f g/m^3 \n", humidityAbs);  
+   Serial.printf("Dew point = %.1f ºC \n", dewpoint);
+   Serial.printf("Battery health: \n");
+   Serial.printf("Output Voltage = %.3f V \n", voltage);
+   Serial.printf("ADC Reading = %d\n", sensorValue);
+   Serial.printf("Battery Percentage = %d \n", bat_percentage); 
+  }
+}
+
+// Pushing data to Thingspeak.com
+void thingSpeak() {
+  // set the fields with the values
+    ThingSpeak.setField(1, tempC);
+    ThingSpeak.setField(2, humidity);
+    ThingSpeak.setField(3, pressureAbs);
+    ThingSpeak.setField(4, humidityAbs);
+    ThingSpeak.setField(5, pressureSea);
+    ThingSpeak.setField(6, dewpoint);
+    ThingSpeak.setField(8, voltage);
+    // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
+    // pieces of information in a channel.  Here, we write to field 1.
+    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+
+    if(x == 200){
+      Serial.println("Channel updated successfully into Thingspeak.");
+      Serial.println("============================================"); 
+    }
+    else{
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+    }
+}
+
 void setup() {
-  Serial.begin(115200);
-  Serial.setTimeout(2000);
-
-  // Wait for serial to initialize.
-  while (!Serial) { }
-
-  Serial.println("Device Started");
-  Serial.println("-------------------------------------");
-  Serial.println("Running Deep Sleep Firmware!");
-  Serial.println("-------------------------------------");
+  if (DEBUG) {
+    Serial.begin(115200);
+    Serial.setTimeout(2000);
+    // Wait for serial to initialize.
+    while (!Serial) { }
+    delay(100);
+    Serial.println("Device Started");
+    Serial.println("-------------------------------------");
+    Serial.println("Running Deep Sleep Firmware!");
+    Serial.println("-------------------------------------");
+  }
 
   // Functions sequences to be run
   initBME280(); //BME280 sensor check
   getDataBME280();
+  BME280_Sleep(sensor_address);
+  batteryLevel();
   printValues();
   ThingSpeak.begin(client); // Initialize ThingSpeak  
   connectWiFi();
   thingSpeak();
-  BME280_Sleep(sensor_address);  
-  Serial.println("Going into deep sleep for 3 minutes!");
-  ESP.deepSleep(3e8); // 30e7 is 3 minutes
-
+  Serial.println("Going into deep sleep for 5 minutes!");
+  ESP.deepSleep(SLEEP_TIME); 
 }
 
 void loop() {
