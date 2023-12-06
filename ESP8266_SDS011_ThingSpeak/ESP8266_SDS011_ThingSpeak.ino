5d96147f6a7b99a048d5574e8acf91d95a7bca4b
Nikolay Georgiev <ngeorgiev@lelypartners.com>
2023-12-06 13:06:28 +0200

    Fixing sleep time issue

--- ESP8266_SDS011_ThingSpeak/ESP8266_SDS011_ThingSpeak.ino
+++ ESP8266_SDS011_ThingSpeak/ESP8266_SDS011_ThingSpeak.ino
@@ -6,8 +6,8 @@
 #include <ArduinoOTA.h>
 #include "SdsDustSensor.h"
 #include "wifi_credentials_consts.h"
-#define WAKE_UP_INTERVAL 3e4 // 3e4 uS is 30 seconds
-#define SLEEP_INTERVAL 60000 // 3e8 uS is 5 minutes
+#define WAKE_UP_INTERVAL 3e4 // 3e4 mS is 30 seconds
+#define SLEEP_INTERVAL 3e5 // 3e5 mS is 5 minutes - delay() takes milliseconds
 #define DEBUG 1 // debug = 1 -> enable
 
 // Serial Comunication with SDS011 sensor
@@ -51,47 +51,45 @@ void printValues(); //Printing values into the serial monitor for debugging purp
 
 // Initilization of SDS011 sensor
 void initSDS011(){
-      sds.begin();
-      ReportingModeResult pm = sds.setQueryReportingMode();
-      Serial.println(sds.queryFirmwareVersion().toString()); // prints firmware version
-      Serial.println(sds.setQueryReportingMode().toString());
+    sds.begin();
+    ReportingModeResult pm = sds.setQueryReportingMode();
+    Serial.println(sds.queryFirmwareVersion().toString()); // prints firmware version
+    Serial.println(sds.setQueryReportingMode().toString());
 }
 
 // Getting data from SDS011
 void getDataSDS011() {
-       sds.wakeup();
-       delay(wakeupInterval); // warm up for 30 seconds
+    sds.wakeup();
+    delay(wakeupInterval); // warm up for 30 seconds
 
-       PmResult pm = sds.queryPm();
-       if (pm.isOk()) {
+    PmResult pm = sds.queryPm();
+    if (pm.isOk()) {
         pm25 = pm.pm25;
         pm10 = pm.pm10;
-       } else {
+    } else {
         Serial.print("Could not read values from sensor, reason: ");
         Serial.println(pm.statusToString());
-       }
-       Serial.println("Before stuck!\n");
-       WorkingStateResult state = sds.sleep();  //Think to fix how not to get stuck!
-       Serial.println("After stuck!\n");
+    }
 
-       if (state.isWorking()) {
+    WorkingStateResult state = sds.sleep();  //Think to fix how not to get stuck!
+
+    if (state.isWorking()) {
         Serial.println("Problem with sleeping the sensor.");
-       } else {
+    } else {
         Serial.println("Sensor is sleeping");
-       }
+    }
 }
 
 void setup() {
-   Serial.begin(115200);
-   setup_wifi();
+    Serial.begin(115200);
+    setup_wifi();
 
-   // Begin OTA
-   //ArduinoOTA.begin(); // Starts OTA
-   //updateOTA(); //WiFi connection and OTA update code
+    // Begin OTA
+    //ArduinoOTA.begin(); // Starts OTA
+    //updateOTA(); //WiFi connection and OTA update code
 
-   // Sensors initialization functions
-   initSDS011(); //SDS011 sensor start  
-
+    // Sensors initialization functions
+    initSDS011(); //SDS011 sensor start  
 }
 
 void loop() {
